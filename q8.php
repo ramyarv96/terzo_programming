8fbfca1186d0a73ab6545782e9cdd6b7bf3211c2
ramyarv96 <96230917+ramyarv96@users.noreply.github.com>
2021-12-16 17:43:06 +0530

    Update q8.php

--- q8.php
+++ q8.php
@@ -21,6 +21,26 @@
 					echo "<br>";  
 				}  
 			?>  
+			<?php  
+				for($i=0;$i<=4;$i++){  
+					for($k=4;$k>=$i;$k--){  
+						echo "  ";  
+					}  
+					for($j=1;$j<=$i;$j++){  
+						echo "*  ";  
+					}  
+					echo "<br>";  
+				}  
+				for($i=3;$i>=1;$i--){  
+					for($k=4;$k>=$i;$k--){  
+						echo "  ";  
+					}  
+					for($j=1;$j<=$i;$j++){  
+						echo "*  ";  
+					}  
+					echo "<br>";  
+				}  
+			?>  
 		</div>
 	</body>
 </html>
