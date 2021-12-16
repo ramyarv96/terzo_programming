370c9597910c4b70248da83a950bbdfc3b061eb9
ramyarv96 <96230917+ramyarv96@users.noreply.github.com>
2021-12-16 20:58:17 +0530

    Update q7.php

--- q7.php
+++ q7.php
@@ -2,25 +2,35 @@
 	<body>
 		<div style="text-align:center;">
 			<?php  
-				for($i=0;$i<=4;$i++){  
-					for($k=4;$k>=$i;$k--){  
-						echo "  ";  
+				if(!empty($_POST['array_size'])){
+					//$arr = $_POST['array_1'];
+					$size = $_POST['array_size'];
+					for($i=0;$i<=$size;$i++){  
+						for($k=$size;$k>=$i;$k--){  
+							echo "  ";  
+						}  
+						for($j=1;$j<=$i;$j++){  
+							echo "*  ";  
+						}  
+						echo "<br>";  
 					}  
-					for($j=1;$j<=$i;$j++){  
-						echo "*  ";  
+					for($i=($size-1);$i>=1;$i--){  
+						for($k=$size;$k>=$i;$k--){  
+							echo "  ";  
+						}  
+						for($j=1;$j<=$i;$j++){  
+							echo "*  ";  
+						}  
+						echo "<br>";  
 					}  
-					echo "<br>";  
-				}  
-				for($i=3;$i>=1;$i--){  
-					for($k=4;$k>=$i;$k--){  
-						echo "  ";  
-					}  
-					for($j=1;$j<=$i;$j++){  
-						echo "*  ";  
-					}  
-					echo "<br>";  
-				}  
+				}
 			?>  
 		</div>
+		<?php if(empty($_POST['array_size'])){ ?>
+		<form action="" method="post">
+			Enter Number : <input type="number" name="array_size" id="array_size" onchange="myFunction();" />
+			<button type="submit">Submit</button>
+		</form>
+		<?php } ?>
 	</body>
 </html>
