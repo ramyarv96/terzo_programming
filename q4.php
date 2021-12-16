64b661521f3b4c5d2e8bdf12703f4f653e9bde4c
ramyarv96 <96230917+ramyarv96@users.noreply.github.com>
2021-12-16 18:14:47 +0530

    Update q4.php

--- q4.php
+++ q4.php
@@ -1,51 +1,30 @@
 <html>
-	<head>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	</head>
 	<body>
 	
 		<?php 
 		
-			$array_size = 0;
-			$duplicates = '';
 			$arr = array();
-			$arrayList = '';
-			if(!empty($_POST['array_size'])){
-				//$arr = $_POST['array_1'];
-				$size = $_POST['array_size'];
-				for($i=0;$i<$size;$i++){
-					$name = "array_".$i;
-					array_push($arr,$_POST[$name]);
-					$arrayList.=$_POST[$name].',';
+			if(!empty($_POST['words'])){
+				$words = $_POST['words'];
+				$split = explode (" ", $words);
+				foreach($split as $k=>$v){
+					if($v!=''){
+						array_push($arr,$v);
+					}
 				}
 				if(sizeof($arr > 0)) {
-					$arrayList = rtrim($arrayList,',');
 					$val = array_count_values($arr);
-					echo "Array : [".$arrayList."]";
+					echo "Word : ".$words;
 					echo "<br />";
 					print_r($val);
 				}
 			}
 		?>
-		<?php if(empty($_POST['array_size'])){ ?>
+		<?php if(empty($_POST['words'])){ ?>
 		<form action="" method="post">
-			Enter size of array : <input type="number" name="array_size" id="array_size" onchange="myFunction();" /><br />
-			<div id="array_input">
-			
-			</div>
+			Enter the words : <input type="text" name="words" id="words" /><br />
+			<button type="submit">Submit</button>
 		</form>
 		<?php } ?>
-		<script>
-			
-			function myFunction(){
-				var size = $("#array_size").val();
-				var html = 'Enter Array of Values : <br />';
-				for(var i=0; i<size; i++){
-					html+='<input type="text" name="array_'+i+'" /><br />';
-				}
-				html+='<button type="submit">Submit</button>';
-				$("#array_input").html(html);
-			}
-		</script>
 	</body>
 </html>
